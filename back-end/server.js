90f37ada11c2c3e61f9e1ac2d523fec321dc200e
romain hippeau <rominch.1993@gmail.com>
2020-10-18 18:31:03 +0200

    initialisation back-end

---
+++ back-end/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+/*var corsOptions = {
+  origin: "http://localhost:8080"
+};*/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome " });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
