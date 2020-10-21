acfd9965f641bde088abf94218471c627ceb10db
romain hippeau <rominch.1993@gmail.com>
2020-10-21 14:34:18 +0200

    mise en place du token et des regles regex

--- back-end/routes/usersCtrl.js
+++ back-end/routes/usersCtrl.js
@@ -2,7 +2,10 @@
 const bcrypt = require('bcrypt');
 const jwtUtils = require('../utils/jwt.utils');
 const models = require('../models');
+//email regex
 
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/
 //routes
 module.exports = {
     register: function(req , req){
@@ -14,6 +17,16 @@ module.exports = {
        if(email == null || username == null || password == null){
            return res.status(400).json({'error': 'missing parameters'})
        }
+       //mise en place des regles regexp et taille de l'username
+       if(username.length >=13 || username.length <=4){
+        return res.status(400).json({'error': 'wrong username (must be length 5 - 12)'})
+    }
+    if (!EMAIL_REGEX.test(email)) {
+        return res.status(400).json({'error': 'email is not valid'})
+    }
+    if (!PASSWORD_REGEX.test(password)) {
+        return res.status(400).json({'error': 'password is not valid'})
+    }
        //recherche pour voir si l'utilisateur est deja dans la base de données
        //si l'utilisateur n'est pas trouver on l'inscris et hash son mot de passe avec bcrypt
        models.user.findOne({
